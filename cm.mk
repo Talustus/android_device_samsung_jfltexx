a6296d89464bd55b2b71f48e5d6e251332d84b7a
root <root@moran-ubuntu.(none)>
2013-05-01 14:29:52 -0500

    Adjust to Sprint

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
+$(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
 
-PRODUCT_NAME := cm_jfltetmo
-PRODUCT_DEVICE := jfltetmo
+PRODUCT_NAME := cm_jfltespr
+PRODUCT_DEVICE := jfltespr
 
